f61547e3156400e91ac6b84af4c61ea52dd79824
alexander-binder <alex-binder@live.com.au>
2022-10-11 16:12:34 +0200

    Created pages programatically with gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,47 @@
+// exports.createPages = async  function({actions, graphql}) {
+//     const {data} = await graphql`
+//         query {
+//             allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+//                 edges {
+//                   node {
+//                     frontmatter {
+//                       slug
+//                     }
+//                     id
+//                   }
+//                 }
+//             }
+//         }
+//     `;
+
+// // Create paginated pages for posts
+
+// const postPerPage = 3;
+
+// const numberOfPages = Math.ceil(data.allMdx.edges.length / postPerPage);
+
+//     Array.from({ length: numberOfPages }).forEach((_, i) => {
+//         actions.createPages({
+//             path: i === 0 ? `/` : `/${i + 1 }`,
+//             component: require.resolve("./src/templates/allPosts.js"),
+//             context: {
+//                 limit: postPerPage,
+//                 skip: i * postPerPage,
+//                 currentPage: i + 1,
+//             }
+//         });
+//     });
+
+// // Create singel blog posts
+//     data.allMdx.edges.forEach(edge => {
+//         const slug = edge.node.frontmatter.slug;
+//         const id = edge.node.id;
+
+//         actions.createPages({
+//             path: slug,
+//             component: equire.resolve("./src/templates/singlePost.js"),
+//             context: {id},
+//         })
+//     })
+
+// }
