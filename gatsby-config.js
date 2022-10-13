ce1d748aee6ad6348765aa967a4a414cd8849cbf
alexander-binder <alex-binder@live.com.au>
2022-10-13 14:21:27 +0200

    added feature Image

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         path: `${__dirname}/src/images`
       } ,
     },
-    `gatsby-plugin-image`,
+    // `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
